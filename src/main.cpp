8492236268f09cdd6ea7c7cd516adc5637e0fb6e
hieunk <khachieunk@gmail.com>
2023-02-24 10:31:27 +0100

    Build project with cmake

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Hieu Nguyen
+ * @brief Main program
+ * @version 0.1
+ * @date 2023-02-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello World\n";
+    return 0;
+}
